e4c3e70f60c48a132472662ba77d2dd3bf126666
George Kibe <georgekibewambui@gmail.com>
2022-06-23 19:02:35 +0300

    customizing settings configuration and media files

---
+++ server/buenas_real_estate/settings/development.py
@@ -0,0 +1,27 @@
+from typing_extensions import dataclass_transform
+
+import django
+from django.db import DJANGO_VERSION_PICKLE_KEY
+from .base import *
+
+# Database
+# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+"""
+DATABASES = {
+    'default': {
+        'ENGINE': env("POSTGRES_ENGINE"),
+        'NAME': env("POSTGRES_DB"),
+        'USER': env("POSTGRES_USER"),
+        'PASSWORD':env("POSTGRES_PASSWORD"),
+        'HOST':env("PG_HOST"),
+        'PORT':env('PG_PORT'),
+    }
+}
+
+"""
