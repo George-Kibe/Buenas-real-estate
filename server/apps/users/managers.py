54770fe3f274a8f91e668b49c0a23ca8e0d0626e
George Kibe <georgekibewambui@gmail.com>
2022-06-24 12:49:10 +0300

    adding signals

--- server/apps/users/managers.py
+++ server/apps/users/managers.py
@@ -35,7 +35,7 @@ class CustomUserManager(BaseUserManager):
         user.save(using=self._db)
         return user
     
-    def create_superuser(self, username, first_name, last_name, email, passoword, **extra_fields):
+    def create_superuser(self, username, first_name, last_name, email, password, **extra_fields):
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
         extra_fields.setdefault("is_active", True)
@@ -45,7 +45,7 @@ class CustomUserManager(BaseUserManager):
         if extra_fields.get("is_superuser") is not True:
             raise ValueError(_("Superuser must have is_superuser=True"))
         
-        if not passoword:
+        if not password:
             raise ValueError(_("Superuser must have a password!"))
 
         if email:
@@ -54,7 +54,7 @@ class CustomUserManager(BaseUserManager):
         else:
             raise ValueError(_("Admin Account: An Email is required"))
         
-        user = self.create_user(username, first_name, last_name, email, passoword, **extra_fields)
+        user = self.create_user(username, first_name, last_name, email, password, **extra_fields)
         user.save(using=self._db)
         return user
 
