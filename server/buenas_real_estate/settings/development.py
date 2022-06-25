b626b04cc8a50535f1e4dec0c1c29b9439af5c55
George Kibe <georgekibewambui@gmail.com>
2022-06-25 14:48:58 +0300

    user serializer and its API urls

--- server/buenas_real_estate/settings/development.py
+++ server/buenas_real_estate/settings/development.py
@@ -1,5 +1,15 @@
 from .base import *
 
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = env("EMAIL_HOST")
+EMAIL_USEE_TLS = True
+EMAIL_PORT = env ("EMAIL_PORT")
+EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = "info@buenasconsultants.co.ke"
+DOMAIN = env("DOMAIN")
+SITE_NAME = "BUENAS REAL ESTATE"
+
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 DATABASES = {
