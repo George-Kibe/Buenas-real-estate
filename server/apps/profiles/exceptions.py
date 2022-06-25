4b90089541d675ebc367d8b1a0577e23e1d46ab7
George Kibe <georgekibewambui@gmail.com>
2022-06-25 16:01:31 +0300

    exceptions and renderers

---
+++ server/apps/profiles/exceptions.py
@@ -0,0 +1,45 @@
+from rest_framework.exceptions import APIException
+
+class ProfileNotFound(APIException):
+    status_code = 404
+    default_detail = "The requested profile does not exist"
+
+class NotYourProfile(APIException):
+    status_code = 403
+    default_detail = "You do not have the necessary priveleges to access this Account!"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
