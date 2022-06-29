12840c2c95b2059966c98aea4cb28cf1ba9081d6
George Kibe <georgekibewambui@gmail.com>
2022-06-29 19:28:26 +0300

    starting react application

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+
+
+function App() {
+  return (
+    <div>
+      <header >
+        <p>Welcome to Buenas real Estate Website</p>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
